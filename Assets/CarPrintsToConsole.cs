f9dbf5a6ff7b619fc58d11a5921ca22697960053
Void7 <limen191100@gmail.com>
2021-12-28 00:19:57 +0300

    Add first scripts
    Add my first scripts on C# that writes text to console.

---
+++ Assets/CarPrintsToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarPrintsToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Car asks: For what purpose you designed me?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TruckPrintsToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckPrintsToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Truck's asking: do u sell ribov?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
